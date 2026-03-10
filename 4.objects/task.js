1f71d5f345979cb184944f9e4da8ee0d6f6bd4e8
Romanova Natalya <natalia7@bk.ru>
2026-03-10 21:27:02 +0300

    Задание Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
 function Student(name, gender, age) {
-  
+    this.name = name,
+    this.gender = gender,
+    this.age = age,
+    this.marks = []
 }
+
+let student1 = new Student("Ivan", "male", 21);
+let student2 = new Student("Oled", "male", 18);
+let student3 = new Student("Anna", "female", 19);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  
+ [this.subject] = [subjectName];
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.hasOwnProperty("marks")) {
+   this.marks.push(...marksToAdd);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.hasOwnProperty("marks")) {
+   return 0;
+  } else {
+    let sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+      sum += this.marks[i];
+   }
+   return sum / this.marks.length;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  [this.excluded].push(reason);
 }
